b62b15a432b770eb4e79a0b869dec52d0477364d
Yichao Yu <yyu10@L-WTRC5ZPG8W-M.local>
2016-12-09 17:17:33 -0500

    fill in address from google geocode response into Address

--- hni-provider/src/main/java/org/hni/provider/service/DefaultGeoCodingService.java
+++ hni-provider/src/main/java/org/hni/provider/service/DefaultGeoCodingService.java
@@ -1,7 +1,13 @@
 package org.hni.provider.service;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.TypeRef;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import org.apache.http.client.fluent.Request;
 import org.hni.user.om.Address;
 import org.springframework.stereotype.Service;
@@ -9,22 +15,29 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class DefaultGeoCodingService implements GeoCodingService {
 
-    private static final String GOOGLE_MAP_API_KEY="AIzaSyBCmt3RMn46CIpxUx20hmlpPbx6ws-lbkI";
+    private static final String GOOGLE_MAP_API_KEY = "AIzaSyBCmt3RMn46CIpxUx20hmlpPbx6ws-lbkI";
+
+    private static final Configuration CONF = Configuration
+            .builder()
+            .mappingProvider(new JacksonMappingProvider())
+            .jsonProvider(new JacksonJsonProvider())
+            .build();
 
     @Override
     public Optional<Address> resolveAddress(String address) {
 
         Address addrPoint = null;
 
-        String targetURI= null;
+        String targetURI = null;
         try {
             targetURI = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-                    URLEncoder.encode(address,"UTF-8") + "&key=" + GOOGLE_MAP_API_KEY;
+                    URLEncoder.encode(address, "UTF-8") + "&key=" + GOOGLE_MAP_API_KEY;
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
             return Optional.empty();
@@ -35,17 +48,24 @@ public class DefaultGeoCodingService implements GeoCodingService {
                     .execute()
                     .returnContent()
                     .asString();
-            if(json != null) {
-                DocumentContext ctx = JsonPath.parse(json);
+            if (json != null) {
+                DocumentContext ctx = JsonPath.using(CONF).parse(json);
 
-                if("OK".equals(ctx.read("status"))) {
+                if ("OK".equals(ctx.read("status"))) {
                     Double latitude = ctx.read("$.results[0].geometry.location.lat");
                     Double longitude = ctx.read("$.results[0].geometry.location.lng");
+                    TypeRef<List<AddressComponent>> type = new TypeRef<List<AddressComponent>>() {
+                    };
+                    List<AddressComponent> addressComponents = ctx.read("$.results[0].address_components", type);
                     addrPoint = new Address();
+                    String streetNumber = getValue(addressComponents, "street_number");
+                    addrPoint.setAddress1((streetNumber == null ? "" : (streetNumber + " ")) + getValue(addressComponents, "route"));
+                    addrPoint.setCity(getValue(addressComponents, "locality"));
+                    addrPoint.setState(getValue(addressComponents, "administrative_area_level_1"));
+                    addrPoint.setZip(getValue(addressComponents, "postal_code"));
                     addrPoint.setLatitude(latitude);
                     addrPoint.setLongitude(longitude);
                 }
-
 
             }
         } catch (IOException e) {
@@ -56,4 +76,24 @@ public class DefaultGeoCodingService implements GeoCodingService {
 
     }
 
+    private String getValue(List<AddressComponent> addressComponents, String typeFilter) {
+        Optional<AddressComponent> addr = addressComponents.stream().filter(ac -> ac.getTypes().contains(typeFilter)).findFirst();
+        return addr.orElse(new AddressComponent()).getShortName();
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class AddressComponent {
+        // a shortcut naming
+        @JsonProperty("short_name")
+        String shortName;
+        List<String> types;
+
+        public String getShortName() {
+            return shortName;
+        }
+
+        public List<String> getTypes() {
+            return types;
+        }
+    }
 }
--- hni-provider/src/test/java/org/hni/provider/service/TestProviderLocationService.java
+++ hni-provider/src/test/java/org/hni/provider/service/TestProviderLocationService.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
-
 import java.util.Collection;
 import java.util.Date;
 
@@ -19,26 +18,27 @@ import static org.junit.Assert.*;
 /**
  * The schema for testing is located in the hni-schema project which gets shared across
  * all test cases.
- * 
- * @author j2parke
  *
+ * @author j2parke
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:test-applicationContext.xml"} )
+@ContextConfiguration(locations = {"classpath:test-applicationContext.xml"})
 @Transactional
 public class TestProviderLocationService {
 
-	@Inject private ProviderService providerService;
-	@Inject private ProviderLocationService providerLocationService;
+    @Inject
+    private ProviderService providerService;
+    @Inject
+    private ProviderLocationService providerLocationService;
 
-	@Test
-	public void testGetProviderLocation() {
-		ProviderLocation providerLocation = providerLocationService.get(1L);
-		assertNotNull(providerLocation);
-	}
+    @Test
+    public void testGetProviderLocation() {
+        ProviderLocation providerLocation = providerLocationService.get(1L);
+        assertNotNull(providerLocation);
+    }
 
 	/*
-	@Test
+    @Test
 	public void testAddAddress() {
 		ProviderLocation providerLocation = providerLocationService.get(1L);
 		assertNotNull(providerLocation);
@@ -54,43 +54,53 @@ public class TestProviderLocationService {
 	}
 	*/
 
-	@Test
-	public void testAddLocationToProvider() {
-		Provider provider = providerService.get(1L);
-		assertNotNull(provider);
+    @Test
+    public void testAddLocationToProvider() {
+        Provider provider = providerService.get(1L);
+        assertNotNull(provider);
 
-		ProviderLocation pl = new ProviderLocation();
-		pl.setCreated(new Date());
-		pl.setCreatedById(1L);
-		pl.setName("test new location");
-		pl.setProvider(provider);
+        ProviderLocation pl = new ProviderLocation();
+        pl.setCreated(new Date());
+        pl.setCreatedById(1L);
+        pl.setName("test new location");
+        pl.setProvider(provider);
 
-		pl = providerLocationService.save(pl);
+        pl = providerLocationService.save(pl);
 
-		Collection<ProviderLocation> list = providerLocationService.with(provider);
-		assertNotNull(list);
-		assertTrue(list.size() > 0);
+        Collection<ProviderLocation> list = providerLocationService.with(provider);
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
 
-	}
+    }
 
-	@Test
-	public void testGetCustomerAddress() throws Exception {
-		Address address = providerLocationService.searchCustomerAddress("bridle view way ohcolumbus");
-		assertNotNull(address);
-	}
+    @Test
+    public void testGetCustomerAddress() throws Exception {
+        Address address = providerLocationService.searchCustomerAddress("bridle view way ohcolumbus");
+        assertNotNull(address);
+        assertTrue("Bridle view way".equalsIgnoreCase(address.getAddress1()));
+        assertTrue("OH".equalsIgnoreCase(address.getState()));
+        assertTrue("columbus".equalsIgnoreCase(address.getCity()));
+    }
 
-//	@Test
-	public void testGetProviderLocationByCustomerId() {
-		Address address = new Address();
-		address.setAddress1("bridle view way");
-		address.setState("oh");
-		address.setCity("columbus");
-		address.setLatitude(-82.9834599);
-		address.setLongitude(40.1376158);
-		Collection<ProviderLocation> providerLocations = providerLocationService.providersNearCustomer(address, 3, 0, 0);
-		assertTrue(providerLocations.size() > 0);
+    @Test
+    public void testGetCustomerAddress_invalid() throws Exception {
+        Address address = providerLocationService.searchCustomerAddress("1 main");
+        assertNotNull(address);
+        assertTrue("1 main".equalsIgnoreCase(address.getAddress1()));
+    }
+
+    //	@Test
+    public void testGetProviderLocationByCustomerId() {
+        Address address = new Address();
+        address.setAddress1("bridle view way");
+        address.setState("oh");
+        address.setCity("columbus");
+        address.setLatitude(-82.9834599);
+        address.setLongitude(40.1376158);
+        Collection<ProviderLocation> providerLocations = providerLocationService.providersNearCustomer(address, 3, 0, 0);
+        assertTrue(providerLocations.size() > 0);
 
 //		providerLocations = providerLocationService.providersNearCustomer("reston town center reston va", 1, 0, 0);
 //		assertTrue(providerLocations.size() > 0);
-	}
+    }
 }
